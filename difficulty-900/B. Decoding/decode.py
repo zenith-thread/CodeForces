80e0d3551005fea896514d4c50e1b8cdf08bba6f
zenith-thread <zenithpkhanif@gmail.com>
2022-06-12 23:49:20 +0500

    Improved Code

--- difficulty-900/B. Decoding/decode.py
+++ difficulty-900/B. Decoding/decode.py
@@ -13,18 +13,15 @@ start = 0
 end = len(string) - 1
 index = []
 
-for i in range(len(string)):
+for i in range(len(string)//2 + 1):
     try:
         index.append((string[-1],end))
         index.append((string[-2],start))
         start += 1
         end -= 1
-        string.pop()
-        string.pop()
+        [string.pop() for i in range(2)]
     except IndexError:
         pass 
 
-pre_proc = len(index) - len(n)
-[index.pop() for i in range(pre_proc) if len(n) % 2 != 0]
 index.sort(key=lambda i:i[1])
 [print(index[i][0],end="") for i in range(len(n))]
